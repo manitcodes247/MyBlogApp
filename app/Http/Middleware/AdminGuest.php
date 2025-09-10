0706b25fb9887a0d2fc78b8f2acd52be868e647f
Manit Singh <manitcodes247@gmail.com>
2025-09-11 02:33:43 +0530

    Update AdminGuest middleware logic

--- app/Http/Middleware/AdminGuest.php
+++ app/Http/Middleware/AdminGuest.php
@@ -9,7 +9,7 @@ class AdminGuest
 {
     public function handle(Request $request, Closure $next){
         // If admin_id exists in session => already logged in, redirect to dashboard
-        if ($request->session()->has('admin_id')) {
+        if($request->session()->has('admin_id')) {
             return redirect()->route('articles.index');
         }
 
